87cf2f7b20e1eb557251f555839ee983fb9967f3
Emma Hack <ehack78@gmail.com>
2020-10-17 10:06:16 +0100

    function 3 completed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -4,9 +4,9 @@ def get_pet_shop_name(pet_shop_info):
 def get_total_cash(pet_shop_info):
     return pet_shop_info["admin"]["total_cash"]
 
-# def add_or_remove_cash(pet_shop_info, cash_to_add_or_remove):
-#     new_total_cash = (pet_shop_info["admin"] ["total_cash"]) + cash_to_add_or_remove
-#     return new_total_cash
+def add_or_remove_cash(pet_shop_info, cash_to_add_or_remove):
+    pet_shop_info["admin"]["total_cash"] += cash_to_add_or_remove
+    
 
 def get_pets_sold(pet_shop_info):
     return pet_shop_info["admin"]["pets_sold"]
@@ -18,3 +18,11 @@ def get_pets_sold(pet_shop_info):
 def get_stock_count(pet_shop_info):
     stock = len(pet_shop_info["pets"])
     return stock 
+
+# def get_pets_by_breed(pet_shop_info, breed_type):
+#     pets = []
+#     for pet in pet_shop_info:
+#         for breed in pet:
+#             if pets[2] == breed_type:
+#                 pets.append((pet_shop_info["pets"]["name"]))
+#     return pets
