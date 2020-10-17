0310136f37779a11da28540a712ffa61ae2e9e40
Emma Hack <ehack78@gmail.com>
2020-10-17 11:17:46 +0100

    function 8 complete

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -18,10 +18,9 @@ def get_stock_count(pet_shop_info):
     stock = len(pet_shop_info["pets"])
     return stock 
 
-# def get_pets_by_breed(pet_shop_info, breed_type):
-#     pets = []
-#     for pet in pet_shop_info:
-#         for breed in pet:
-#             if pets[2] == breed_type:
-#                 pets.append((pet_shop_info["pets"]["name"]))
-#     return pets
+def get_pets_by_breed(pet_shop_info, breed_type):
+    breed_list = []
+    for pet in pet_shop_info["pets"]:
+        if pet["breed"] == breed_type:
+            breed_list.append(pet["breed"])
+    return breed_list
