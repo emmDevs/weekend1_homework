7bd3ba6c251525d3fb01a81bf2e93fa0201e4222
Emma Hack <ehack78@gmail.com>
2020-10-17 17:31:02 +0100

    function 21 updated

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -58,8 +58,13 @@ def customer_can_afford_pet(customers, new_pet):
             return True
         return False
 
-def sell_pet_to_customer(pet_shop_info, pet, customers):
-    customers["pets"].append(pet)
+def sell_pet_to_customer(pet_shop_info, pet, customer):
+    customer["pets"].append(pet)
     pet_shop_info["admin"]["pets_sold"] += 1
-    customers["cash"] -= pet["price"]
+    customer["cash"] -= pet["price"]
     pet_shop_info["admin"]["total_cash"] += pet["price"]
+
+
+
+ 
+
