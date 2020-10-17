2ea0801979cd2966f8cc8b2dbc6f9e77bbcfa365
Emma Hack <ehack78@gmail.com>
2020-10-17 10:10:21 +0100

    functions 4 & 6 completed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -11,9 +11,8 @@ def add_or_remove_cash(pet_shop_info, cash_to_add_or_remove):
 def get_pets_sold(pet_shop_info):
     return pet_shop_info["admin"]["pets_sold"]
 
-# def increase_pets_sold(get_pets_sold, number_pets_sold):
-#     total_pets_sold = get_pets_sold(pet_shop_info) + number_pets_sold
-#     return total_pets_sold
+def increase_pets_sold(pet_shop_info, number_pets_sold):
+    pet_shop_info["admin"]["pets_sold"] += number_pets_sold
 
 def get_stock_count(pet_shop_info):
     stock = len(pet_shop_info["pets"])
